e69b02742ff03c3e5c58adf00f8214807e8d5c89
OleksiiMikhieiev <babybong0993@gmail.com>
2023-02-27 01:43:59 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,8 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+console.log(galleryItems);
+
 const gallery = document.querySelector('.gallery');
 const cardsMarkup = createImgCardsMarkup(galleryItems);
 
