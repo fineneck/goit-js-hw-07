c8bf9d67748008c0daece2864c65c0f79678ec6f
OleksiiMikhieiev <babybong0993@gmail.com>
2023-02-27 01:42:18 +0200

    02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,38 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const cardsMarkup = createImgCardsMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', cardsMarkup);
+gallery.addEventListener('click', onPictureClick);
+
+function createImgCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image"
+      src="${preview}" 
+      alt="${description}" 
+      /></a>
+      `;
+    })
+    .join('');
+}
+
+function onPictureClick(evt) {
+  evt.preventDefault();
+
+  const galleryImage = evt.target.classList.contains('gallery__image');
+
+  if (!galleryImage) {
+    return;
+  }
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
